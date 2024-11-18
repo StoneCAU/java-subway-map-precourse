2b543d88b062684e2f17d41f3e475016f004941d
StoneCAU <seokwoo1999@naver.com>
2024-11-19 03:10:19 +0900

    feat(Line): Line 생성 시 검증 로직 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -2,27 +2,23 @@ package subway.domain;
 
 import java.util.LinkedList;
 import java.util.List;
+import subway.exception.ErrorMessage;
+import subway.exception.SubwayException;
+import subway.repository.LineRepository;
 import subway.repository.StationRepository;
 
 public class Line {
     private String name;
     private List<Station> stations = new LinkedList<>();
 
-    public Line(String name, String upLine, String downLine) {
-        createStations(upLine, downLine);
+    public Line(String name, String upStationName, String downStationName) {
+        validateDuplicate(upStationName, downStationName);
+        createStations(upStationName, downStationName);
         this.name = name;
     }
 
     public String getName() {
         return name;
-    }
-
-    private void createStations(String upLine, String downLine) {
-        Station upStation = StationRepository.findByName(upLine).orElse(Station.addLineToStation(name));
-        Station downStation = StationRepository.findByName(downLine).orElse(Station.addLineToStation(name));
-
-        stations.add(upStation);
-        stations.add(downStation);
     }
 
     public void addStation(String stationName, int index) {
@@ -36,5 +32,42 @@ public class Line {
             return stations.contains(station);
         }
         return false;
+    }
+
+    public static void validateName(String name) {
+        if (isDuplicateName(name)) {
+            throw new SubwayException(ErrorMessage.DUPLICATE_LINE_NAME);
+        }
+    }
+
+    public static void validateDuplicate(String upStationName, String downStationName) {
+        if (isDuplicateUpStationAndDownStation(upStationName, downStationName)) {
+            throw new SubwayException(ErrorMessage.DUPLICATE_UPSTATION_AND_DOWNSTATION);
+        }
+    }
+
+    private void createStations(String upStationName, String downStationName) {
+        Station upStation = StationRepository.findByName(upStationName).orElseGet(() -> {
+            Station temp = Station.addLineToStation(upStationName);
+            StationRepository.addStation(temp);
+            return temp;
+        });
+        Station downStation = StationRepository.findByName(downStationName).orElseGet(() -> {
+            Station temp = Station.addLineToStation(downStationName);
+            StationRepository.addStation(temp);
+            return temp;
+        });
+
+        stations.add(upStation);
+        stations.add(downStation);
+    }
+
+    private static boolean isDuplicateName(String name) {
+        Line line = LineRepository.findLineByName(name).orElse(null);
+        return line != null;
+    }
+
+    private static boolean isDuplicateUpStationAndDownStation(String upStationName, String downStationName) {
+        return upStationName.equals(downStationName);
     }
 }
