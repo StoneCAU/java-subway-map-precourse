095dbdc24d353280687c7ce4dcf642f3e6942342
StoneCAU <seokwoo1999@naver.com>
2024-11-18 16:36:08 +0900

    feat(ErrorMessage, InputValidator): 역 등록 시 input 값 검증 구현
    - 2글자 이름만 가능하도록

--- src/main/java/subway/exception/ErrorMessage.java
+++ src/main/java/subway/exception/ErrorMessage.java
@@ -1,7 +1,8 @@
 package subway.exception;
 
 public enum ErrorMessage {
-    INVALID_MENU_INPUT("선택할 수 없는 기능입니다.");
+    INVALID_MENU_INPUT("선택할 수 없는 기능입니다."),
+    INVALID_STATION_NAME_LENGTH("역 이름은 2글자 이상이어야 합니다.");
 
     private final String message;
 
--- src/main/java/subway/validator/InputValidator.java
+++ src/main/java/subway/validator/InputValidator.java
@@ -4,9 +4,24 @@ import subway.exception.ErrorMessage;
 import subway.exception.SubwayException;
 
 public class InputValidator {
-    public static void validateMenu(String input) {
+    private static final String QUIT = "Q";
+    private static final String BACK = "B";
+    private static final int MIN_LENGTH = 2;
+
+    public static void validateMainMenu(String input) {
+        if (isQuitMenu(input)) return;
         isNumber(input);
-        validateQuitMenu(input);
+    }
+
+    public static void validateStationMenu(String input) {
+        if (isBackMenu(input)) return;
+        isNumber(input);
+    }
+
+    public static void validateStationName(String input) {
+        if (!isValidLength(input)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_STATION_NAME_LENGTH.getMessage());
+        }
     }
 
     private static void isNumber(String input) {
@@ -17,13 +32,15 @@ public class InputValidator {
         }
     }
 
-    private static void validateQuitMenu(String input) {
-        if (isQuitMenu(input)) {
-            throw new SubwayException(ErrorMessage.INVALID_MENU_INPUT);
-        }
+    private static boolean isQuitMenu(String input) {
+        return input.equals(QUIT);
     }
 
-    private static boolean isQuitMenu(String input) {
-        return input.equals("Q");
+    private static boolean isBackMenu(String input) {
+        return input.equals(BACK);
+    }
+
+    private static boolean isValidLength(String input) {
+        return input.length() >= MIN_LENGTH;
     }
 }
